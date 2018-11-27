06c298feaa167bf53e57e3e702006983081b0c2d
NguyenVietThai <nguyenvietthai1351997@gmail.com>
2018-11-27 21:40:05 +0700

    1 file client to send data client to client

--- src/test1/client.c
+++ src/test1/client.c
@@ -16,11 +16,15 @@ int main(int argc, char *argv[]){
 	char buff[BUFF_SIZE];
 	struct sockaddr_in server_addr; /* server's address information */
 	int msg_len, bytes_sent, bytes_received;
+	int c;
 	
 	if(argc !=3){
 		printf("PARAMETER INVALID!\n");
 		return 0;
 	}
+	printf("Chon quyen:\n1.Guitruoc-Nhansau\n2.Guisau-Nhantruoc\n");
+	printf("Nhap:");scanf("%d",&c);
+	printf("[%d]\n",c);
 
 	//Step 1: Construct socket
 	client_sock = socket(AF_INET,SOCK_STREAM,0);
@@ -35,36 +39,68 @@ int main(int argc, char *argv[]){
 		printf("\nERROR!CANNOT CONNECT TO CLIENT! CLIENT EXIT IMEDIATELY! ");
 		return 0;
 	}
-	//Step 4: Communicate with server			
-	while(1){
-		//send message
-		printf("\nINPUT	:");
-		memset(buff,'\0',(strlen(buff)+1));
-		fgets(buff, BUFF_SIZE, stdin);
-		msg_len = strlen(buff);
-		if (msg_len == 1){
-			printf("EXIT!\n");
-			break;
+	//Step 4: Communicate with server	
+	if(c == 1){
+		while(getchar()!='\n');
+		while(1){
+			//send message
+			printf("\nSEND	:");
+			memset(buff,'\0',(strlen(buff)+1));
+			fgets(buff, BUFF_SIZE, stdin);
+			msg_len = strlen(buff);
+			// if (msg_len == ){
+			// 	printf("EXIT!\n");
+			// 	break;
+			// }
+			
+			bytes_sent = send(client_sock, buff, msg_len, 0);
+			if(bytes_sent <= 0){
+				printf("\nCONNECTION CLOSED!\n");
+				break;
+			}
+			
+			//receive echo reply
+			bytes_received = recv(client_sock, buff, BUFF_SIZE-1, 0);
+			if(bytes_received <= 0){
+				printf("\nCANNOT RECEIVE DATA FROM SERVER!\n");
+				break;
+			}
+			
+			buff[bytes_received] = '\0';
+			printf("----------------\n");
+			printf("MESSAGE	: [%s]\n", buff);
 		}
-		
-		bytes_sent = send(client_sock, buff, msg_len, 0);
-		if(bytes_sent <= 0){
-			printf("\nCONNECTION CLOSED!\n");
-			break;
+	}else{
+		while(getchar()!='\n');
+		while(1){
+			//receive echo reply
+			bytes_received = recv(client_sock, buff, BUFF_SIZE-1, 0);
+			if(bytes_received <= 0){
+				printf("\nCANNOT RECEIVE DATA FROM SERVER!\n");
+				break;
+			}
+			
+			buff[bytes_received] = '\0';
+			printf("----------------\n");
+			printf("MESSAGE	: [%s]\n", buff);
+			
+			//send message
+			printf("\nSEND	:");
+			memset(buff,'\0',(strlen(buff)+1));
+			fgets(buff, BUFF_SIZE, stdin);
+			msg_len = strlen(buff);
+			// if (msg_len == 1){
+			// 	printf("EXIT!\n");
+			// 	break;
+			// }
+			
+			bytes_sent = send(client_sock, buff, msg_len, 0);
+			if(bytes_sent <= 0){
+				printf("\nCONNECTION CLOSED!\n");
+				break;
+			}
 		}
-		
-		//receive echo reply
-		bytes_received = recv(client_sock, buff, BUFF_SIZE-1, 0);
-		if(bytes_received <= 0){
-			printf("\nCANNOT RECEIVE DATA FROM SERVER!\n");
-			break;
-		}
-		
-		buff[bytes_received] = '\0';
-		printf("----------------\n");
-		printf("SERVER-MESSAGE	: [%s]\n", buff);
 	}
-	
 	//Step 4: Close socket
 	close(client_sock);
 	return 0;
