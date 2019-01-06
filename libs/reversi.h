22d28c655d86a1fc1b5f9362d34689e9a676cdff
nguyenvietthai <nguyenvietthai1351997@gmail.com>
2019-01-06 13:37:21 +0700

    up lai code game moi

---
+++ libs/reversi.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BLACK  1
+#define NONE   0
+#define WHITE -1
+
+int** board;         
+                 
+int size;            
+int current = BLACK; 
+int count;          
+
+void initialize (const int n);
+void finalize();
+void display();
+int isReversible ( const int m, const int n );
+int reverse( int m, int n );
+int winner();
